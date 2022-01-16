cbb3223724c6f5c2f30a2a211672bd22d5d9e253
Suprit <suprit@Suprits-MacBook-Pro.local>
2022-01-16 15:22:02 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World 
-        </p>
+        <p>Hello World</p>
+        <p>Hello world twice</p>
       </header>
     </div>
   );
