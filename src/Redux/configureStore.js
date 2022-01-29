ac33af13fec1b18f48d6f488f0390364894d5e50
Suprit <suprit@Suprits-MacBook-Pro.local>
2022-01-29 15:59:27 -0500

    thunk configuration

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -4,6 +4,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
   const composeEnhancer =
@@ -11,6 +12,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancer(applyMiddleware(reduxImmutableStateInvariant()))
+    composeEnhancer(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,39 +5,14 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    //dispatching an action to create a course
-    // debugger;
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h2>Add Courses</h2>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
-
+      <>
+        <h2>Courses</h2>
         {this.props.courses.map((course) => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
